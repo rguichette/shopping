8e567a5ce1a4444877ce84cddbabe3095ee9aa11
rguichette <ralphgui@gmail.com>
2022-05-30 22:31:10 -0400

    sidebar init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const activeMenu = true;
 
   return (
     <div className="App">
-     <h1 className="underline text-3xl">hi</h1>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg" re>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div> 
+          :
+          <div className="w-0 dark:bg-secondary-dark-bg">sidebar</div>}
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import * as ReactDOM from 'react-dom';
 
 import App from './App'

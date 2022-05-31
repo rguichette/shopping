acd19f6fb4c4eb672580af23647f1ec13753512a
rguichette <ralphgui@gmail.com>
2022-05-31 02:17:27 -0400

    completed folder structure init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -22,9 +22,55 @@ function App() {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div> 
-          :
-          <div className="w-0 dark:bg-secondary-dark-bg">sidebar</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">sidebar</div>
+          )}
+
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md-ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"></div>
+          </div>
+            <Routes>
+              <Route path="/" element="test" />
+              {/* Dashboard */}
+              <Route path="/ecommerce" element="test" />
+              <Route path="/ecomerce" element="ecomerce" />
+
+              {/* pages */}
+              <Route path="/orders" element="Orders"/>
+              <Route path="/employees" element="Employees"/>
+              <Route path="/Customers" element="Custumers"/>
+
+              {/* apps */}
+              <Route path="/kanban" element="Kanban"/>
+              <Route path="/editor" element="editor"/>
+              <Route path="/calendar" element="calendar"/>
+              <Route path="/color-picker" element="ColorPiciker"/>
+            
+            {/* charts */}
+              <Route path="/line" element="line"/>
+              <Route path="/area" element="Area"/>
+              <Route path="/bar" element="Bar"/>
+              <Route path="/pie" element="Pie"/>
+              <Route path="/financial" element="Financial"/>
+              <Route path="/color-mapping" element="ColorMapping"/>
+              <Route path="/pyramid" element="Pyramid"/>
+              <Route path="/stacked" element="Stacked"/>
+            
+            
+            
+            </Routes>
+
+          <div>
+
+          </div>
         </div>
       </BrowserRouter>
     </div>
---
+++ src/components/Charts/LineCharts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LineCharts = () => {
+  return (
+    <div>LineCharts</div>
+  )
+}
+
+export default LineCharts
+
+
+import React from 'react'
+

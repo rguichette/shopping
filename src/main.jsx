be13698d930f92376502ba8187538bc457851ef9
rguichette <ralphgui@gmail.com>
2022-05-31 11:05:04 -0400

    added context and basic sidebar func

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,12 @@ import * as ReactDOM from 'react-dom';
 import App from './App'
 import './index.css'
 
+
+import {ContextProvider} from './contexts/ContextProvider'
+
 ReactDOM.render(<React.StrictMode>
+  <ContextProvider>
+
     <App />
+  </ContextProvider>
   </React.StrictMode>, document.getElementById('root'))

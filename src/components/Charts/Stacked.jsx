15088b3bb2be0a31fb3f6e34e55a45dd722d5109
rguichette <ralphgui@gmail.com>
2022-06-01 10:22:54 -0400

    ecom chart complete

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
-const Stacked = () => {
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip  } from '@syncfusion/ej2-react-charts'
+import {stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis} from '../../data/dummy'
+const Stacked = ({width, height}) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent 
+    width={width}
+     height={height} 
+     background="white"
+     id="charts"
+     primaryXAxis={stackedPrimaryXAxis}
+     primaryYAxis={stackedPrimaryYAxis}
+    chartArea={{border: {width:0}}}
+    tooltip={{enable:true}}
+
+     >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index)=>{
+          return <SeriesDirective key={index} {...item} />
+        })}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
-export default Stacked
+export default Stacked 

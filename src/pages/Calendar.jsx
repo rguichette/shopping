fde1c36d1ee4c45c5feeb5e2af41a4e45ea7cb9e
rguichette <ralphgui@gmail.com>
2022-06-02 00:13:47 -0400

    added kanban

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {ScheduleComponent, ViewsDirective, ViewDirective,Month, Day, Week, WorkWeek, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule'
+
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+import {scheduleData} from '../data/dummy'
+import {Header} from '../components'
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className="m-2 md:m-1 mt-24 p-2 md:p-10 bg-white rounded-3xl" >
+
+    <Header category={"App"} title="Calendar"/>
+    <ScheduleComponent 
+    height={"650px"}
+    selectedDate ={new Date(2021, 0, 10)}
+    eventSettings={{
+      dataSource: scheduleData
+    }}
+
+    >
+
+
+  <Inject services={[Day, Week, WorkWeek, Month,Agenda, Resize, DragAndDrop ]}/>
+    </ScheduleComponent>
+
+    </div>
   )
 }
 

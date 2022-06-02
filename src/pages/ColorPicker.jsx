9325126cc202a20af94a50f4ce2066b32a754010
rguichette <ralphgui@gmail.com>
2022-06-02 00:35:56 -0400

    added color picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {ColorPickerComponent} from "@syncfusion/ej2-react-inputs"
+
+import {Header} from '../components'
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className="m-2 md:m-1 mt-24 p-2 md:p-10 bg-white rounded-3xl" >
+
+    <Header category={"App"} title="ColorPicker"/>
+    <div className='text-center'>
+
+      <div id='preview'/>
+      <div className='flex justify-center  item-center gap-20 flex-wrap'>
+      <div>
+        <p className='text-2xl font-simibold mt-2 mb-4'>Inline Pallete</p>
+        <ColorPickerComponent id="inline-pallete" mode='Palette' modeSwitcher={false} inline showButtons={false}/>
+      </div>
+      <div>
+        <p className='text-2xl font-simibold mt-2 mb-4'>Inline Picker</p>
+        <ColorPickerComponent id="inline-pallete" mode='Picker' modeSwitcher={false} inline showButtons={false}/>
+      </div>
+      </div>
+      
+    </div>
+    
+    </div>
   )
 }
 
